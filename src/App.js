36d6ca3f646ae356505269fe9ec09cd50da105e8
Hemavathypandiarajan <23suca35@tcarts.in>
2026-03-19 20:53:04 +0530

    CSS changed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-//my project frontend
-
 import React from "react";
 import CourseList from "./CourseList";
 import EnrollmentList from "./EnrollmentList";
 
 function App() {
   return (
-    <div style={{ padding: "1rem" }}>
-      <h1>Course Enrollment</h1>
-      <CourseList />
-      <hr />
-      <EnrollmentList />
+    <div className="app-container">
+      <h1 className="main-heading">Course Enrollment</h1>
+
+      <div className="content">
+        <CourseList />
+        <EnrollmentList />
+      </div>
     </div>
   );
 }
